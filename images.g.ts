e2d781d33a21b4f94118e84d8bdd6a118ab77056
sbergeroncp <93004917+sbergeroncp@users.noreply.github.com>
2024-04-26 10:02:34 -0400

    Fichiers initiaux pour un projet MakeCode

---
+++ images.g.ts
@@ -0,0 +1,44 @@
+// Code généré automatiquement. Ne pas modifier.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "spaceship_1":return img`
+. . . . . . 9 9 . . . . . . . . 
+. . . . . 9 1 1 9 . . . . . . . 
+. . . . 9 1 1 1 1 9 . . . . . . 
+. . . . 1 1 6 a 1 1 . . . . . . 
+. . . . 1 6 c c a 1 . . . . . . 
+. . . . 1 1 c c 1 1 . . . . . . 
+. . . 1 1 1 1 1 1 1 1 . . . . . 
+. . . 9 6 1 1 1 1 a 9 . . . . . 
+. . 9 6 f 1 1 1 1 f a 9 . . . . 
+. . 9 f f 1 1 1 1 f f 9 . . . . 
+. 9 6 f f 1 1 1 1 f f a 9 . . . 
+. 9 f f f 1 1 1 1 f f f 9 . . . 
+9 6 f f f 1 1 1 1 f f f a 9 . . 
+9 6 f . f f f f f f . f a 9 . . 
+d . . . . a a a a . . . . 9 . . 
+. . . . . 4 5 5 4 . . . . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Code généré automatiquement. Ne pas modifier.
